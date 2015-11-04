5d8b554e0f255f3ffe9d3d6b3d14088760609aca
Bartek Kruszczynski <kruszczyk@gmail.com>
2015-11-04 02:06:43 +0100

    Losing battle with UnmarshalJSON

--- farm.go
+++ farm.go
@@ -1,90 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
-	"os"
 	"net/http"
+	"io"
+	"io/ioutil"
+	"fmt"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-type species int
+var animals = []*animal{}
 
-const (
-	cow species = iota
-	pig
-	cock
-	tit
-	deer
-	sheep
-)
-
-const fileName string = "farm.json"
-
-var speciesNames = [6]string{
-	"Cow",
-	"Pig",
-	"Cock",
-	"Tit",
-	"Deer",
-	"Sheep",
-}
-
-// MarshalJSON intr
-func (species species) MarshalJSON() ([]byte, error) {
-	return json.Marshal(speciesNames[species])
-}
-
-// Animal is a representation of an animal
-type animal struct {
-	Species species
-	Name    string
-	Age     uint
-}
-
-func readFarm() []*animal {
-	animals := []*animal{}
-	for shouldContinue := "y"; shouldContinue == "y"; {
-		// name
-		animal := &animal{}
-		fmt.Print("Animal's name: ")
-		fmt.Scan(&animal.Name)
-		// species
-		fmt.Print("Animal's species (0=Cow,1=Pig,2=Cock,3=Tit,4=Deer,5=Sheep): ")
-		fmt.Scanf("%d", &animal.Species)
-		animal.Species = animal.Species % species(len(speciesNames))
-		// age
-		fmt.Print("Animal's age: ")
-		fmt.Scanf("%d", &animal.Age)
-		animals = append(animals, animal)
-		// continue
-		fmt.Print("Type 'y' to continue, anything else to exit: ")
-		fmt.Scan(&shouldContinue)
-	}
-	return animals
-}
-
-func serializeFarm(animals []*animal) error {
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	encoder := json.NewEncoder(file)
+func serializeAnimals(writer io.Writer) error {
+	encoder := json.NewEncoder(writer)
 	if err := encoder.Encode(animals); err != nil {
 		return err
 	}
-	fmt.Println("Farm data saved successfully to", fileName)
 	return nil
 }
 
-func main() {
-	if err := serializeFarm(readFarm()); err != nil {
-		panic(err)
-	}
-  // http.HandleFunc("/", handler)
-  // http.ListenAndServe(":8080", nil)
+// GetAnimals renders animals list
+func getAnimals(w http.ResponseWriter, _r *http.Request, _ httprouter.Params) {
+	serializeAnimals(w)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func createAnimal(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	animal := &animal{}
+	// animal.Name = r.FormValue("name")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(body))
+	json.Unmarshal(body, &animal)
+	animals = append(animals, animal)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/animals", getAnimals)
+	router.POST("/animals", createAnimal)
+	http.ListenAndServe(":8080", router)
 }
